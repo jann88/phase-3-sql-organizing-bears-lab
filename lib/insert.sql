8da363d28fb26b08ab0bdf092f445d4f22190ce6
Janet Ngei <janet.ngei@student.moringaschool.com>
2022-08-22 23:05:55 +0300

    complete

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 30, 'M', 'black', 'sanguine', 'True');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 31, 'F', 'black', 'sanguine', 'True');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 32, 'M', 'black', 'sanguine', 'True');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 33, 'F', 'black', 'sanguine', 'True');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 34, 'M', 'black', 'sanguine', 'True');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 35, 'F', 'black', 'sanguine', 'True');
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 36, 'M', 'black', 'sanguine', 'True');
+INSERT INTO bears (name) VALUES (NULL);
